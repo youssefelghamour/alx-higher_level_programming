6f33fffff27096804b174ce6cece7022ca789e8b
youssef <youssefelghamour@gmail.com>
2023-11-04 06:51:34 -0700

    correct 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for row in matrix:
+        if len(row) == 0:
+            print()
         for element in row:
             print("{:d}".format(element), end=' ')
         print()
