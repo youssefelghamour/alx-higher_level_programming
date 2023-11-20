fa0062a218678026085078017f6f7669971c97d5
youssef <youssefelghamour@gmail.com>
2023-11-20 03:20:01 -0800

    add 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb_printed = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except:
+            break
+        else:
+            nb_printed += 1
+
+    print()
+    return (nb_printed)
