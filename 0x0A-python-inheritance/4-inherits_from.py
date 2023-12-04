0ee01a5f1242e2fa13a3b3a35d7c37f9c93a6678
youssef <youssefelghamour@gmail.com>
2023-12-04 07:20:54 -0800

    fix typo in 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """ Module for inherits_from method """
 
 
-def is_same_class(obj, a_class):
+def inherits_from(obj, a_class):
     """ Function that returns True if the object is an instance of a class
         that inherited (directly or indirectly) from the specified class;
         otherwise False
