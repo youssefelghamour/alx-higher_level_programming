0b1eb9437b6ea48a16d5dad4e3ab157403ad370d
youssef <youssefelghamour@gmail.com>
2023-12-04 07:19:42 -0800

    add 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module for inherits_from method """
+
+
+def is_same_class(obj, a_class):
+    """ Function that returns True if the object is an instance of a class
+        that inherited (directly or indirectly) from the specified class;
+        otherwise False
+
+    Args:
+        obj: object
+        a_class: class type
+
+    Returns:
+        True if obj is and instance of a_class
+        False if not
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
