a7243cd1cce03f7f0fd7d6b9a514a50d648506f1
youssef <youssefelghamour@gmail.com>
2024-01-17 12:44:40 -0800

    add 101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- list all genres not linked to the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+    SELECT DISTINCT tv_genres.id
+    FROM tv_genres
+    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id AND tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
+
