0218ac649d1931413b50333f07e253be7e833a99
youssef <youssefelghamour@gmail.com>
2024-02-12 07:36:36 -0800

    add 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+if (arg.length <= 2) {
+  console.log('No argument');
+} else if (arg.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
