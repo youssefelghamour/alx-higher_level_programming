06958125b27ae7b11d459cc452491e7fa5f55100
youssef <youssefelghamour@gmail.com>
2024-02-12 09:43:46 -0800

    correct 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,20 @@
 
 const arg = process.argv;
 let max = parseInt(arg[2]);
+let secondMax = -Infinity;
+
 if (!max || (arg.length === 3)) {
   console.log(0);
 } else {
   for (let i = 2; i < arg.length; i++) {
     const num = parseInt(arg[i]);
     if (num > max) {
+      secondMax = max;
       max = num;
+    } else if (num > secondMax && num < max) {
+      secondMax = num;
     }
   }
-  console.log(max);
+  console.log(secondMax);
 }
+
