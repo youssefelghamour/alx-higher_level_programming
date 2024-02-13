75715f95bf3ce84fd87c73ec2de8841644e41b99
youssef <youssefelghamour@gmail.com>
2024-02-13 07:45:28 -0800

    correct 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Square = require('./5-square.js');
+const Square1 = require('./5-square.js');
 
-class Square extends Square {
+class Square extends Square1 {
   charPrint (c) {
     let char = 'X';
     if (c) {
