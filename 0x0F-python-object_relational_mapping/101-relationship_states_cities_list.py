a9f8175105ef02343204705a8230c5468369706b
youssef <youssefelghamour@gmail.com>
2024-02-17 09:40:43 -0800

    add 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,25 +3,23 @@
     contained in the database hbtn_0e_101_usa
 """
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 if __name__ == '__main__':
     database_url = 'mysql://{}:{}@localhost:3306/{}'.\
                     format(argv[1], argv[2], argv[3])
-    engine = create_engine(database_url)
-
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    s = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
 
-    for state in s:
+    for state in st:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
