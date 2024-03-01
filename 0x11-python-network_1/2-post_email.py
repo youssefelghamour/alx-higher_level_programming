ec6ffaa8578999bf3047169eb2f3d1ca56a415cf
youssef <youssefelghamour@gmail.com>
2024-03-01 02:27:52 -0800

    correct 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" script that takes in a URL and an email, sends a POST request to the passed URL
-    with the email as a parameter, and displays the body of the response (decoded in utf-8)
+""" script that takes in a URL and an email, sends a POST request to the passed
+    URL with the email as a parameter, and displays the body of the response
 """
 import urllib.request
 import sys
@@ -8,12 +8,12 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {'email' : sys.argv[2]}
-    
+    values = {'email': sys.argv[2]}
+
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
-    
+
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
         print(body)
