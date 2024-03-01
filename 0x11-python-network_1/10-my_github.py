ac4764e662ceffe928dce3ba1a83d6c2111dcd4b
youssef <youssefelghamour@gmail.com>
2024-03-01 11:56:01 -0800

    change 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,11 +7,17 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/user'
     user = argv[1]
     token = argv[2]
+    url = "https://api.github.com/users/{}".format(user)
 
-    r = requests.get(url, auth=requests.auth.HTTPBasicAuth(user, token))
+    headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": "Bearer {}".format(token),
+            "X-GitHub-Api-Version": "2022-11-28"
+               }
+
+    r = requests.get(url, headers=headers)
 
     json_data = r.json()
     print(json_data.get('id'))
