bd2b02ea7ed38687db4cb989e0524de64c1e5786
youssef <youssefelghamour@gmail.com>
2024-03-01 08:04:39 -0800

    add 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    payload = {'q': ""}
+
+    try:
+        payload = {'q': argv[1]}
+    except:
+        pass
+
+    url = "http://0.0.0.0:5000/search_user"
+    
+    r = requests.post(url, data=payload)
+
+    try:
+        body_json = r.json()
+        if body_json:
+            print("[{}] {}".format(body_json.get('id'), body_json.get('name')))
+        else:
+            print("No result")
+    except:
+            print("Not a valid JSON")
