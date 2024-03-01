69794da717169f71ec305cfc5f5071d6946f1aa1
youssef <youssefelghamour@gmail.com>
2024-03-01 07:06:27 -0800

    add 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status with requests """
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
