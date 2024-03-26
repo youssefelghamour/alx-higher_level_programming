c5ccf3202d7a1e7b5df9635deadc689d4c73bdbe
youssef <youssefelghamour@gmail.com>
2024-03-26 10:03:39 -0700

    7. Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+
+    for (const charUrl of characters) {
+      request(charUrl, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
