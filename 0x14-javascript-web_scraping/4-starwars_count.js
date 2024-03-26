38124fd219152ebb731f0303574dc32bbe1f512e
youssef <youssefelghamour@gmail.com>
2024-03-26 09:23:16 -0700

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = process.argv[2];
 
+const apiUrl = process.argv[2];
 const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+let count = 0;
 
 request(apiUrl, function (error, response, body) {
   if (!error) {
     const filmsData = JSON.parse(body).results;
-    let count = 0;
 
     for (const film of filmsData) {
       for (const character of film.characters) {
@@ -18,6 +19,6 @@ request(apiUrl, function (error, response, body) {
         }
       }
     }
-    console.log(count);
   }
+  console.log(count);
 });
