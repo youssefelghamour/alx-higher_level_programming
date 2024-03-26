ea0e1bcf012d961cccb4adfa394471f161aa01f9
youssef <youssefelghamour@gmail.com>
2024-03-26 08:31:50 -0700

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const url = process.argv[2];
 
 request(url, (error, response) => {
   if (!error) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
