c9ebd6e6437456862d23f3a9f0e3a1d6d1d21154
youssef <youssefelghamour@gmail.com>
2024-04-03 08:05:47 -0700

    add 101-script.js

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,14 @@
+const $ = window.$;
+window.onload = function () {
+  $('#add_item').click(function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+
+  $('#remove_item').click(function () {
+    $('ul.my_list li:last-child').remove();
+  });
+
+  $('#clear_list').click(function () {
+    $('ul.my_list').empty();
+  });
+};
